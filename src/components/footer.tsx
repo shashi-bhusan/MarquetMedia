f51634793447df287039c02a9c3d12967c484af6
ShahidMollick <shahidmollick13@gmail.com>
2025-08-26 01:18:48 +0530

    Fix typo in footer copyright notice

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -226,7 +226,7 @@ export default function Footer() {
         >
           
             <p className="font-montserrat text-foreground text-sm my-8">
-            © 2025 Marquet Media. All rights reserved. Maintained by <a href="https://beenait.uk/" target="_blank" rel="noopener noreferrer" className="hover:underline">BeenaIT Solutions</a>.
+            © 2025 Marquet Media. All rights reserved. Maintained by <a href="https://beenait.uk/" target="_blank" rel="noopener noreferrer" className="hover:underline">beenaIT Solutions</a>.
             </p>
         </div>
       </div>
